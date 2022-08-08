c2a6f38841558ad024dc21080470fc947befda26
Akimov Eduard <95487679+akimoveduard@users.noreply.github.com>
2022-08-08 19:37:51 +0300

    refactor: regexp для avatar

--- utils/regexp.js
+++ utils/regexp.js
@@ -1,4 +1,4 @@
-const imgUrlRegExp = /^(https?):\/\/(www.)?([-.~:/?#[\]@!$&'()*+,;=\w])+$/;
+const imgUrlRegExp = /^https?://([a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|jpeg|gif|png|wepb)$/gi;
 
 module.exports = {
   imgUrlRegExp,
