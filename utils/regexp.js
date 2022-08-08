813a1ff835eb729f2be0ce98a6928fbe8da430c9
Akimov Eduard <95487679+akimoveduard@users.noreply.github.com>
2022-08-08 19:48:16 +0300

    Update regexp.js

--- utils/regexp.js
+++ utils/regexp.js
@@ -1,4 +1,4 @@
-const imgUrlRegExp = /^https?://([a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|jpeg|gif|png|wepb)$/gi;
+const imgUrlRegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/;
 
 module.exports = {
   imgUrlRegExp,
